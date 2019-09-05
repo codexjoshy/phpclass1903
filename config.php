60e7493d3608bfb663977bb790ad513c07361ae2
codexjoshy <joshuaebhoriao@gmail.com>
2019-09-05 12:04:46 +0100

    edited a page

--- config.php
+++ config.php
@@ -1,3 +1,16 @@
 <?php 
 	define('URL', 'http://localhost/wd1903/');
+	define('DIRECTORY', 'C:/wamp64/www');
+	//phpmyadmin connection
+
+	define('HOST', 'localhost');
+	define('USERNAME', 'root');
+	define('PASSWORD', '');
+	define('DBASE', 'school');
+	//connect to database
+	$dbconnect = mysqli_connect(HOST, USERNAME, PASSWORD, DBASE);
+	if (!$dbconnect) {
+		die().mysqli_error($dbconnect) ;
+	}
+	
 ?>
